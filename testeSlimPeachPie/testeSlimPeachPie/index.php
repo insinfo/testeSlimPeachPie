57a1851d9e628a7685dee009c28c3a68476779ba
Isaque Neves <insinfo2008@gmail.com>
2020-10-14 00:14:24 -0300

    Update index.php

--- testeSlimPeachPie/testeSlimPeachPie/index.php
+++ testeSlimPeachPie/testeSlimPeachPie/index.php
@@ -8,6 +8,10 @@ $app->get('/', function ($request, $response, $args) {
     return $response->getBody()->write("Hello ");
 });
 
+$app->get('/teste/t', function ($request, $response, $args) {
+    return $response->getBody()->write("Hello word");
+});
+
 $app->run();
 
 
